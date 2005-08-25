f5c082f663b2fa18108a9efbe2e9ae3667b3a336
Lori Corbani <Lori.Corbani@jax.org>
2005-08-25 13:57:33 +0000

    Fantom3

---
+++ fantom3load.py
@@ -0,0 +1,433 @@
+#!/usr/local/bin/python
+
+#
+# Program: fantom3load.py
+#
+# Original Author: Lori Corbani
+#
+# Purpose:
+#
+#	To load new Probes into:
+#
+#	PRB_Probe
+#	PRB_Reference
+#	ACC_Accession
+#	ACC_AccessionReference
+#
+# Requirements Satisfied by This Program:
+#
+# Usage:
+#	program.py
+#	-S = database server
+#	-D = database
+#	-U = user
+#	-P = password file
+#	-M = mode
+#	-I = input file
+#
+# Envvars:
+#
+# Inputs:
+#
+#	A tab-delimited file in the format:
+#		field 1:  Probe Name
+#		field 2:  MGI ID of Clone
+#		field 3:  Reference (J:#####)
+#		field 4:  Library Name
+#		field 5:  Region Covered
+#		field 6:  Insert Site
+#		field 7:  Insert Size
+#		field 8:  Accession ID (Sequence DB:####|...)
+#		field 9:  Created By
+#
+# Outputs:
+#
+#       4 BCP files:
+#
+#       PRB_Probe.bcp                   master Probe records
+#       PRB_Reference.bcp         	Probe Reference records
+#       ACC_Accession.bcp               Accession records
+#       ACC_AccessionReference.bcp      Accession Reference records
+#
+#       Diagnostics file of all input parameters and SQL commands
+#       Error file
+#
+# Exit Codes:
+#
+# Assumes:
+#
+#	That no one else is adding such records to the database.
+#
+# Bugs:
+#
+# Implementation:
+#
+
+import sys
+import os
+import string
+import getopt
+import accessionlib
+import db
+import mgi_utils
+import loadlib
+import sourceloadlib
+
+#globals
+
+DEBUG = 0		# if 0, not in debug mode
+TAB = '\t'		# tab
+CRT = '\n'		# carriage return/newline
+
+bcpon = 1		# can the bcp files be bcp-ed into the database?  default is yes.
+
+diagFile = ''		# diagnostic file descriptor
+errorFile = ''		# error file descriptor
+inputFile = ''		# file descriptor
+probeFile = ''          # file descriptor
+refFile = ''            # file descriptor
+accFile = ''            # file descriptor
+accRefFile = ''         # file descriptor
+
+probeTable = 'PRB_Probe'
+refTable = 'PRB_Reference'
+accTable = 'ACC_Accession'
+accRefTable = 'ACC_AccessionReference'
+
+probeFileName = probeTable + '.bcp'
+refFileName = refTable + '.bcp'
+accFileName = accTable + '.bcp'
+accRefFileName = accRefTable + '.bcp'
+
+diagFileName = ''	# diagnostic file name
+errorFileName = ''	# error file name
+passwordFileName = ''	# password file name
+
+mode = ''		# processing mode (load, preview)
+probeKey = 0            # PRB_Probe._Probe_key
+refKey = 0		# PRB_Reference._Reference_key
+accKey = 0              # ACC_Accession._Accession_key
+
+mgiTypeKey = 3		# Molecular Segment
+vectorType = 'Phagemid'
+segmentType = 'cDNA'
+
+loaddate = loadlib.loaddate
+
+# Purpose: displays correct usage of this program
+# Returns: nothing
+# Assumes: nothing
+# Effects: exits with status of 1
+# Throws: nothing
+ 
+def showUsage():
+    usage = 'usage: %s -S server\n' % sys.argv[0] + \
+        '-D database\n' + \
+        '-U user\n' + \
+        '-P password file\n' + \
+        '-M mode\n'
+
+    exit(1, usage)
+ 
+# Purpose: prints error message and exits
+# Returns: nothing
+# Assumes: nothing
+# Effects: exits with exit status
+# Throws: nothing
+
+def exit(
+    status,          # numeric exit status (integer)
+    message = None   # exit message (string)
+    ):
+
+    if message is not None:
+        sys.stderr.write('\n' + str(message) + '\n')
+ 
+    try:
+        diagFile.write('\n\nEnd Date/Time: %s\n' % (mgi_utils.date()))
+        errorFile.write('\n\nEnd Date/Time: %s\n' % (mgi_utils.date()))
+        diagFile.close()
+        errorFile.close()
+	inputFile.close()
+    except:
+        pass
+
+    db.useOneConnection(0)
+    sys.exit(status)
+ 
+# Purpose: process command line options
+# Returns: nothing
+# Assumes: nothing
+# Effects: initializes global variables
+#          calls showUsage() if usage error
+#          exits if files cannot be opened
+# Throws: nothing
+
+def init():
+    global diagFile, errorFile, inputFile, errorFileName, diagFileName, passwordFileName
+    global mode
+    global probeFile, refFile, accFile, accRefFile
+ 
+    try:
+        optlist, args = getopt.getopt(sys.argv[1:], 'S:D:U:P:M:I:')
+    except:
+        showUsage()
+ 
+    #
+    # Set server, database, user, passwords depending on options specified
+    #
+ 
+    server = ''
+    database = ''
+    user = ''
+    password = ''
+ 
+    for opt in optlist:
+        if opt[0] == '-S':
+            server = opt[1]
+        elif opt[0] == '-D':
+            database = opt[1]
+        elif opt[0] == '-U':
+            user = opt[1]
+        elif opt[0] == '-P':
+            passwordFileName = opt[1]
+        elif opt[0] == '-M':
+            mode = opt[1]
+        elif opt[0] == '-I':
+            inputFileName = opt[1]
+        else:
+            showUsage()
+
+    # User must specify Server, Database, User and Password
+    password = string.strip(open(passwordFileName, 'r').readline())
+    if server == '' or database == '' or user == '' or password == '' \
+	or mode == '' or inputFileName == '':
+        showUsage()
+
+    # Initialize db.py DBMS parameters
+    db.set_sqlLogin(user, password, server, database)
+    db.useOneConnection(1)
+ 
+    fdate = mgi_utils.date('%m%d%Y')	# current date
+    head, tail = os.path.split(inputFileName) 
+    diagFileName = tail + '.' + fdate + '.diagnostics'
+    errorFileName = tail + '.' + fdate + '.error'
+
+    try:
+        diagFile = open(diagFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % diagFileName)
+		
+    try:
+        errorFile = open(errorFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % errorFileName)
+		
+    try:
+        inputFile = open(inputFileName, 'r')
+    except:
+        exit(1, 'Could not open file %s\n' % inputFileName)
+
+    try:
+        probeFile = open(probeFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % probeFileName)
+
+    try:
+        refFile = open(refFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % refFileName)
+
+    try:
+        accFile = open(accFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % accFileName)
+
+    try:
+        accRefFile = open(accRefFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % accRefFileName)
+
+    # Log all SQL
+    db.set_sqlLogFunction(db.sqlLogAll)
+
+    # Set Log File Descriptor
+    db.set_sqlLogFD(diagFile)
+
+    diagFile.write('Start Date/Time: %s\n' % (mgi_utils.date()))
+    diagFile.write('Server: %s\n' % (server))
+    diagFile.write('Database: %s\n' % (database))
+    diagFile.write('User: %s\n' % (user))
+
+    errorFile.write('Start Date/Time: %s\n\n' % (mgi_utils.date()))
+
+    return
+
+# Purpose: verify processing mode
+# Returns: nothing
+# Assumes: nothing
+# Effects: if the processing mode is not valid, exits.
+#	   else, sets global variables
+# Throws:  nothing
+
+def verifyMode():
+
+    global DEBUG
+
+    if mode == 'preview':
+        DEBUG = 1
+        bcpon = 0
+    elif mode != 'full':
+        exit(1, 'Invalid Processing Mode:  %s\n' % (mode))
+
+# Purpose:  sets global primary key variables
+# Returns:  nothing
+# Assumes:  nothing
+# Effects:  sets global primary key variables
+# Throws:   nothing
+
+def setPrimaryKeys():
+
+    global probeKey, refKey, accKey
+
+    results = db.sql('select maxKey = max(_Probe_key) + 1 from PRB_Probe', 'auto')
+    probeKey = results[0]['maxKey']
+
+    results = db.sql('select maxKey = max(_Reference_key) + 1 from PRB_Reference', 'auto')
+    refKey = results[0]['maxKey']
+
+    results = db.sql('select maxKey = max(_Accession_key) + 1 from ACC_Accession', 'auto')
+    accKey = results[0]['maxKey']
+
+# Purpose:  BCPs the data into the database
+# Returns:  nothing
+# Assumes:  nothing
+# Effects:  BCPs the data into the database
+# Throws:   nothing
+
+def bcpFiles():
+
+    bcpdelim = "|"
+
+    if DEBUG or not bcpon:
+        return
+
+    probeFile.close()
+    refFile.close()
+    accFile.close()
+    accRefFile.close()
+
+    bcpI = 'cat %s | bcp %s..' % (passwordFileName, db.get_sqlDatabase())
+    bcpII = '-c -t\"|" -S%s -U%s' % (db.get_sqlServer(), db.get_sqlUser())
+
+    bcp1 = '%s%s in %s %s' % (bcpI, probeTable, probeFileName, bcpII)
+    bcp2 = '%s%s in %s %s' % (bcpI, refTable, refFileName, bcpII)
+    bcp3 = '%s%s in %s %s' % (bcpI, accTable, accFileName, bcpII)
+    bcp4 = '%s%s in %s %s' % (bcpI, accRefTable, accRefFileName, bcpII)
+
+    for bcpCmd in [bcp1, bcp2, bcp3, bcp4]:
+	diagFile.write('%s\n' % bcpCmd)
+	os.system(bcpCmd)
+
+    return
+
+# Purpose:  processes data
+# Returns:  nothing
+# Assumes:  nothing
+# Effects:  verifies and processes each line in the input file
+# Throws:   nothing
+
+def processFile():
+
+    global probeKey, refKey, accKey
+
+    vectorKey = sourceloadlib.verifyVectorType(vectorType, 0, errorFile)
+    segmentTypeKey = sourceloadlib.verifySegmentType(segmentType, 0, errorFile)
+
+    lineNum = 0
+    # For each line in the input file
+
+    for line in inputFile.readlines():
+
+        error = 0
+        lineNum = lineNum + 1
+
+        # Split the line into tokens
+        tokens = string.split(line[:-1], '\t')
+
+        try:
+	    name = tokens[0]
+	    mgiID = tokens[1]
+	    jnum = tokens[2]
+	    library = tokens[3]
+	    regionCovered = tokens[4]
+	    insertSite = tokens[5]
+	    insertSize = tokens[6]
+	    sequenceIDs = tokens[7]
+	    createdBy = tokens[8]
+        except:
+            exit(1, 'Invalid Line (%d): %s\n' % (lineNum, line))
+
+        referenceKey = loadlib.verifyReference(jnum, lineNum, errorFile)
+	libraryKey = sourceloadlib.verifyLibrary(library, lineNum, errorFile)
+	userKey = loadlib.verifyUser(createdBy, lineNum, errorFile)
+
+	# sequence IDs
+	seqAccDict = {}
+	for seqID in string.split(sequenceIDs, '|'):
+	    if len(seqID) > 0:
+	        [logicalDB, acc] = string.split(seqID, ':')
+	        logicalDBKey = loadlib.verifyLogicalDB(logicalDB, lineNum, errorFile)
+	        if logicalDBKey > 0:
+		    seqAccDict[acc] = logicalDBKey
+
+        if vectorKey == 0 or segmentTypeKey == 0 \
+	   or referenceKey == 0 or userKey == 0 or libraryKey == 0:
+            # set error flag to true
+            error = 1
+
+        # if errors, continue to next record
+        if error:
+            continue
+
+        # if no errors, process the probe
+
+        probeFile.write('%d|%s||%s|%s|%s|||%s|%s|%s||%s|%s|%s|%s\n' \
+            % (probeKey, name, libraryKey, vectorKey, segmentTypeKey, mgi_utils.prvalue(regionCovered), \
+	    mgi_utils.prvalue(insertSite), mgi_utils.prvalue(insertSize), userKey, userKey, loaddate, loaddate))
+
+        refFile.write('%s|%s|%s|0|0|%s|%s|%s|%s\n' % (refKey, probeKey, referenceKey, userKey, userKey, loaddate, loaddate))
+
+        # MGI Accession ID of clone
+	prefixPart, numericPart = accessionlib.split_accnum(mgiID)
+
+        accFile.write('%s|%s|%s|%s|1|%d|%d|0|1|%s|%s|%s|%s\n' \
+            % (accKey, mgiID, prefixPart, numericPart, probeKey, mgiTypeKey, userKey, userKey, loaddate, loaddate))
+
+        accKey = accKey + 1
+
+	# sequence accession ids
+	for acc in seqAccDict.keys():
+	    prefixPart, numericPart = accessionlib.split_accnum(acc)
+            accFile.write('%s|%s|%s|%s|%s|%d|%d|0|1|%s|%s|%s|%s\n' \
+                % (accKey, acc, prefixPart, numericPart, seqAccDict[acc], probeKey, mgiTypeKey, userKey, userKey, loaddate, loaddate))
+            accRefFile.write('%s|%s|%s|%s|%s|%s\n' \
+                % (accKey, referenceKey, userKey, userKey, loaddate, loaddate))
+	    accKey = accKey + 1
+
+	refKey = refKey + 1
+        probeKey = probeKey + 1
+
+    #	end of "for line in inputFile.readlines():"
+
+#
+# Main
+#
+
+init()
+verifyMode()
+setPrimaryKeys()
+processFile()
+bcpFiles()
+exit(0)
+
---
+++ primerload.py
@@ -0,0 +1,499 @@
+#!/usr/local/bin/python
+
+# $Header$
+# $Name$
+
+#
+# Program: primerload.py
+#
+# Original Author: Lori Corbani
+#
+# Purpose:
+#
+#	To load new Primers into:
+#
+#	PRB_Probe
+#	PRB_Marker
+#	PRB_Reference
+#	ACC_Accession
+#
+# Requirements Satisfied by This Program:
+#
+# Usage:
+#	program.py
+#	-S = database server
+#	-D = database
+#	-U = user
+#	-P = password file
+#	-M = mode
+#	-I = input file
+#
+# Envvars:
+#
+# Inputs:
+#
+#	A tab-delimited file in the format:
+#		field 1: MGI Marker Accession ID
+#		field 2: Primer Name
+#		field 3: Reference (J:#####)
+#		field 4: Region Covered
+#		field 5: Sequence 1
+#		field 6: Sequence 2
+#		field 7: Repeat Unit
+#		field 8: More Than One Product (y/n)
+#		field 9: Product Size
+#
+# Outputs:
+#
+#       4 BCP files:
+#
+#       PRB_Probe.bcp                   master Primer records
+#	PRB_Marker.bcp			Primer/Marker records
+#       PRB_Reference.bcp         	Primer Reference records
+#       ACC_Accession.bcp               Accession records
+#
+#       Diagnostics file of all input parameters and SQL commands
+#       Error file
+#
+# Exit Codes:
+#
+# Assumes:
+#
+#	That no one else is adding Nomen records to the database.
+#
+# Bugs:
+#
+# Implementation:
+#
+
+import sys
+import os
+import string
+import getopt
+import db
+import mgi_utils
+import accessionlib
+
+#globals
+
+DEBUG = 0		# if 0, not in debug mode
+TAB = '\t'		# tab
+CRT = '\n'		# carriage return/newline
+
+bcpon = 1		# can the bcp files be bcp-ed into the database?  default is yes.
+
+diagFile = ''		# diagnostic file descriptor
+errorFile = ''		# error file descriptor
+inputFile = ''		# file descriptor
+primerFile = ''         # file descriptor
+markerFile = ''		# file descriptor
+refFile = ''            # file descriptor
+accFile = ''            # file descriptor
+
+primerTable = 'PRB_Probe'
+markerTable = 'PRB_Marker'
+refTable = 'PRB_Reference'
+accTable = 'ACC_Accession'
+
+primerFileName = primerTable + '.bcp'
+markerFileName = markerTable + '.bcp'
+refFileName = refTable + '.bcp'
+accFileName = accTable + '.bcp'
+
+diagFileName = ''	# diagnostic file name
+errorFileName = ''	# error file name
+passwordFileName = ''	# password file name
+
+mode = ''		# processing mode (load, preview)
+primerKey = 0           # PRB_Probe._Probe_key
+refKey = 0		# PRB_Reference._Reference_key
+accKey = 0              # ACC_Accession._Accession_key
+mgiKey = 0              # ACC_AccessionMax.maxNumericPart
+
+dnaType = 'primer'	# PRB_Probe.DNAtype
+relationship = 'A'	# PRB_Marker.relationship
+NA = -2			# for Not Applicable fields
+mgiTypeKey = 3		# Molecular Segment
+mgiPrefix = "MGI:"
+
+referenceDict = {}      # dictionary of references for quick lookup
+markerDict = {}      	# dictionary of markers for quick lookup
+
+cdate = mgi_utils.date('%m/%d/%Y')	# current date
+
+# Purpose: displays correct usage of this program
+# Returns: nothing
+# Assumes: nothing
+# Effects: exits with status of 1
+# Throws: nothing
+ 
+def showUsage():
+    usage = 'usage: %s -S server\n' % sys.argv[0] + \
+        '-D database\n' + \
+        '-U user\n' + \
+        '-P password file\n' + \
+        '-M mode\n'
+
+    exit(1, usage)
+ 
+# Purpose: prints error message and exits
+# Returns: nothing
+# Assumes: nothing
+# Effects: exits with exit status
+# Throws: nothing
+
+def exit(
+    status,          # numeric exit status (integer)
+    message = None   # exit message (string)
+    ):
+
+    if message is not None:
+        sys.stderr.write('\n' + str(message) + '\n')
+ 
+    try:
+        diagFile.write('\n\nEnd Date/Time: %s\n' % (mgi_utils.date()))
+        errorFile.write('\n\nEnd Date/Time: %s\n' % (mgi_utils.date()))
+        diagFile.close()
+        errorFile.close()
+	inputFile.close()
+    except:
+        pass
+
+    db.useOneConnection(0)
+    sys.exit(status)
+ 
+# Purpose: process command line options
+# Returns: nothing
+# Assumes: nothing
+# Effects: initializes global variables
+#          calls showUsage() if usage error
+#          exits if files cannot be opened
+# Throws: nothing
+
+def init():
+    global diagFile, errorFile, inputFile, errorFileName, diagFileName, passwordFileName
+    global mode
+    global primerFile, markerFile, refFile, accFile
+ 
+    try:
+        optlist, args = getopt.getopt(sys.argv[1:], 'S:D:U:P:M:I:')
+    except:
+        showUsage()
+ 
+    #
+    # Set server, database, user, passwords depending on options specified
+    #
+ 
+    server = ''
+    database = ''
+    user = ''
+    password = ''
+ 
+    for opt in optlist:
+        if opt[0] == '-S':
+            server = opt[1]
+        elif opt[0] == '-D':
+            database = opt[1]
+        elif opt[0] == '-U':
+            user = opt[1]
+        elif opt[0] == '-P':
+            passwordFileName = opt[1]
+        elif opt[0] == '-M':
+            mode = opt[1]
+        elif opt[0] == '-I':
+            inputFileName = opt[1]
+        else:
+            showUsage()
+
+    # User must specify Server, Database, User and Password
+    password = string.strip(open(passwordFileName, 'r').readline())
+    if server == '' or database == '' or user == '' or password == '' \
+	or mode == '' or inputFileName == '':
+        showUsage()
+
+    # Initialize db.py DBMS parameters
+    db.set_sqlLogin(user, password, server, database)
+    db.useOneConnection(1)
+ 
+    fdate = mgi_utils.date('%m%d%Y')	# current date
+    head, tail = os.path.split(inputFileName) 
+    diagFileName = tail + '.' + fdate + '.diagnostics'
+    errorFileName = tail + '.' + fdate + '.error'
+
+    try:
+        diagFile = open(diagFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % diagFileName)
+		
+    try:
+        errorFile = open(errorFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % errorFileName)
+		
+    try:
+        inputFile = open(inputFileName, 'r')
+    except:
+        exit(1, 'Could not open file %s\n' % inputFileName)
+
+    try:
+        primerFile = open(primerFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % primerFileName)
+
+    try:
+        markerFile = open(markerFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % markerFileName)
+
+    try:
+        refFile = open(refFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % refFileName)
+
+    try:
+        accFile = open(accFileName, 'w')
+    except:
+        exit(1, 'Could not open file %s\n' % accFileName)
+
+    # Log all SQL
+    db.set_sqlLogFunction(db.sqlLogAll)
+
+    # Set Log File Descriptor
+    db.set_sqlLogFD(diagFile)
+
+    diagFile.write('Start Date/Time: %s\n' % (mgi_utils.date()))
+    diagFile.write('Server: %s\n' % (server))
+    diagFile.write('Database: %s\n' % (database))
+    diagFile.write('User: %s\n' % (user))
+
+    errorFile.write('Start Date/Time: %s\n\n' % (mgi_utils.date()))
+
+    return
+
+# Purpose: verify processing mode
+# Returns: nothing
+# Assumes: nothing
+# Effects: if the processing mode is not valid, exits.
+#	   else, sets global variables
+# Throws:  nothing
+
+def verifyMode():
+
+    global DEBUG
+
+    if mode == 'preview':
+        DEBUG = 1
+        bcpon = 0
+    elif mode != 'load':
+        exit(1, 'Invalid Processing Mode:  %s\n' % (mode))
+
+
+# Purpose:  verify Marker Accession ID
+# Returns:  Marker Key if Marker is valid, else 0
+# Assumes:  nothing
+# Effects:  verifies that the Marker exists either in the marker dictionary or the database
+#	writes to the error file if the Marker is invalid
+#	adds the marker id and key to the marker dictionary if the Marker is valid
+# Throws:  nothing
+
+def verifyMarker(
+    markerID, 	# Accession ID of the Marker (string)
+    lineNum	# line number (integer)
+    ):
+
+    global markerDict
+
+    markerKey = 0
+
+    if markerDict.has_key(markerID):
+        errorFile.write('Duplicate Mouse Marker (%d) %s\n' % (lineNum, markerID))
+    else:
+        results = db.sql('select _Object_key from MRK_Acc_View where accID = "%s" ' % (markerID), 'auto')
+
+        for r in results:
+            if r['_Object_key'] is None:
+                errorFile.write('Invalid Mouse Marker (%d) %s\n' % (lineNum, markerID))
+                markerKey = 0
+            else:
+                markerKey = r['_Object_key']
+                markerDict[markerID] = markerKey
+
+    return markerKey
+
+# Purpose:  verifies the input reference (J:)
+# Returns:  the primary key of the reference or 0 if invalid
+# Assumes:  nothing
+# Effects:  verifies that the Reference exists by checking the referenceDict
+#	dictionary for the reference ID or the database.
+#	writes to the error file if the Reference is invalid.
+#	adds the Reference ID/Key to the global referenceDict dictionary if the
+#	reference is valid.
+# Throws:
+
+def verifyReference(
+    referenceID,          # reference accession ID; J:#### (string)
+    lineNum		  # line number (integer)
+    ):
+
+    global referenceDict
+
+    if referenceDict.has_key(referenceID):
+        referenceKey = referenceDict[referenceID]
+    else:
+        referenceKey = accessionlib.get_Object_key(referenceID, 'Reference')
+        if referenceKey is None:
+            errorFile.write('Invalid Reference (%d): %s\n' % (lineNum, referenceID))
+            referenceKey = 0
+        else:
+            referenceDict[referenceID] = referenceKey
+
+    return(referenceKey)
+
+# Purpose:  sets global primary key variables
+# Returns:  nothing
+# Assumes:  nothing
+# Effects:  sets global primary key variables
+# Throws:   nothing
+
+def setPrimaryKeys():
+
+    global primerKey, refKey, accKey, mgiKey
+
+    results = db.sql('select maxKey = max(_Probe_key) + 1 from PRB_Probe', 'auto')
+    primerKey = results[0]['maxKey']
+
+    results = db.sql('select maxKey = max(_Reference_key) + 1 from PRB_Reference', 'auto')
+    refKey = results[0]['maxKey']
+
+    results = db.sql('select maxKey = max(_Accession_key) + 1 from ACC_Accession', 'auto')
+    accKey = results[0]['maxKey']
+
+    results = db.sql('select maxKey = maxNumericPart + 1 from ACC_AccessionMax ' + \
+        'where prefixPart = "%s"' % (mgiPrefix), 'auto')
+    mgiKey = results[0]['maxKey']
+
+# Purpose:  BCPs the data into the database
+# Returns:  nothing
+# Assumes:  nothing
+# Effects:  BCPs the data into the database
+# Throws:   nothing
+
+def bcpFiles():
+
+    bcpdelim = "|"
+
+    if DEBUG or not bcpon:
+        return
+
+    primerFile.close()
+    markerFile.close()
+    refFile.close()
+    accFile.close()
+
+    bcpI = 'cat %s | bcp %s..' % (passwordFileName, db.get_sqlDatabase())
+    bcpII = '-c -t\"|" -S%s -U%s' % (db.get_sqlServer(), db.get_sqlUser())
+    truncateDB = 'dump transaction %s with truncate_only' % (db.get_sqlDatabase())
+
+    bcp1 = '%s%s in %s %s' % (bcpI, primerTable, primerFileName, bcpII)
+    bcp2 = '%s%s in %s %s' % (bcpI, markerTable, markerFileName, bcpII)
+    bcp3 = '%s%s in %s %s' % (bcpI, refTable, refFileName, bcpII)
+    bcp4 = '%s%s in %s %s' % (bcpI, accTable, accFileName, bcpII)
+
+    for bcpCmd in [bcp1, bcp2, bcp3, bcp4]:
+	diagFile.write('%s\n' % bcpCmd)
+	os.system(bcpCmd)
+	db.sql(truncateDB, None)
+
+    return
+
+# Purpose:  processes data
+# Returns:  nothing
+# Assumes:  nothing
+# Effects:  verifies and processes each line in the input file
+# Throws:   nothing
+
+def processFile():
+
+    global primerKey, refKey, accKey, mgiKey
+
+    lineNum = 0
+    # For each line in the input file
+
+    for line in inputFile.readlines():
+
+        error = 0
+        lineNum = lineNum + 1
+
+        # Split the line into tokens
+        tokens = string.split(line[:-1], '\t')
+
+        try:
+	    markerID = tokens[0]
+	    name = tokens[1]
+	    jnum = tokens[2]
+	    regionCovered = tokens[3]
+	    sequence1 = tokens[4]
+	    sequence2 = tokens[5]
+	    repeatUnit = tokens[6]
+	    moreProduct = tokens[7]
+	    productSize = tokens[8]
+        except:
+            exit(1, 'Invalid Line (%d): %s\n' % (lineNum, line))
+
+	markerKey = verifyMarker(markerID, lineNum)
+        referenceKey = verifyReference(jnum, lineNum)
+
+	if moreProduct == 'y':
+		moreProduct = '1'
+	else:
+		moreProduct = '0'
+
+        if markerKey == 0 or referenceKey == 0:
+            # set error flag to true
+            error = 1
+
+        # if errors, continue to next record
+        if error:
+            continue
+
+        # if no errors, process the primer
+
+        primerFile.write('%d|%s||%d|%d|%s|%s|%s||||%s|%s|%s|%s|%s|%s\n' \
+            % (primerKey, name, NA, NA, sequence1, sequence2, mgi_utils.prvalue(regionCovered), \
+	    dnaType, mgi_utils.prvalue(repeatUnit), mgi_utils.prvalue(productSize), moreProduct, cdate, cdate))
+
+        markerFile.write('%d|%d|%s|%s|%s\n' % (primerKey, markerKey, relationship, cdate, cdate))
+
+        refFile.write('%d|%d|%d|0|0|%s|%s\n' % (refKey, primerKey, referenceKey, cdate, cdate))
+
+        # MGI Accession ID for the marker
+
+        accFile.write('%d|%s%d|%s|%s|1|%d|%d|0|1|%s|%s|%s\n' \
+            % (accKey, mgiPrefix, mgiKey, mgiPrefix, mgiKey, primerKey, mgiTypeKey, cdate, cdate, cdate))
+
+        accKey = accKey + 1
+        mgiKey = mgiKey + 1
+	refKey = refKey + 1
+        primerKey = primerKey + 1
+
+    #	end of "for line in inputFile.readlines():"
+
+    #
+    # Update the AccessionMax value
+    #
+
+    if not DEBUG:
+        db.sql('exec ACC_setMax %d' % (lineNum), None)
+
+#
+# Main
+#
+
+init()
+verifyMode()
+setPrimaryKeys()
+processFile()
+bcpFiles()
+exit(0)
+
+# $Log$
